1513ec63e4d43327e371481a083fa3bfe6925c3b
nkopali <nils.kopali20@live.com>
2018-07-06 16:51:35 +0200

    A little bit of build in constructor-I'm not sure

--- src/ab3/impl/GagglGundackerKopali/BTreeMapImpl.java
+++ src/ab3/impl/GagglGundackerKopali/BTreeMapImpl.java
@@ -7,64 +7,73 @@ import java.util.List;
 
 public class BTreeMapImpl extends BNode implements BTreeMap {
     private int minimalVal;
-    private int called =0;
+    private int called = 0;
     private int size;
     private BNode root;
+    private int children;
+    private boolean leaf;
+    private int key[];
 
-    public BTreeMapImpl(){
+
+    public BTreeMapImpl() {
+        root=new BNode();
+        key= new int[minimalVal];
+        leaf = true;
+        children = 0;
 
     }
 
     @Override
     public void setMinDegree(int t) throws IllegalStateException, IllegalArgumentException {
-        if(t<2) throw new IllegalArgumentException("Must be greater than 2");
-        if(called!=0) throw new IllegalStateException("Already called");//checking if it is called, this called value will be changed also at clear method
-        minimalVal=t-1;//Setting the minimal value
+        if (t < 2) throw new IllegalArgumentException("Must be greater than 2");
+        if (called != 0)
+            throw new IllegalStateException("Already called");//checking if it is called, this called value will be changed also at clear method
+        minimalVal = t - 1;//Setting the minimal value
         called++;
     }
 
     @Override
     public boolean put(int key, String value) throws IllegalStateException, IllegalArgumentException {
-        if(called==0)throw new IllegalStateException();
+        if (called == 0) throw new IllegalStateException();
         return false;
     }
 
     @Override
     public boolean delete(int key) throws IllegalStateException, IllegalArgumentException {
-        if(called==0)throw new IllegalStateException();
+        if (called == 0) throw new IllegalStateException();
         return false;
     }
 
     @Override
     public String contains(int key) throws IllegalStateException {
-        if(called==0)throw new IllegalStateException();
-        int i=0;
+        if (called == 0) throw new IllegalStateException();
+        int i = 0;
 
         return null;
     }
 
     @Override
     public BNode getRoot() throws IllegalStateException {
-        if(called==0)throw new IllegalStateException();
+        if (called == 0) throw new IllegalStateException();
         return null;
     }
 
     @Override
     public List<Integer> getKeys() throws IllegalStateException {
-        if(called==0)throw new IllegalStateException();
+        if (called == 0) throw new IllegalStateException();
         return null;
     }
 
     @Override
     public List<String> getValues() throws IllegalStateException {
-        if(called==0)throw new IllegalStateException();
+        if (called == 0) throw new IllegalStateException();
         return null;
     }
 
     @Override
     public void clear() {
-        called=0;
-        size=0;
+        called = 0;
+        size = 0;
 
     }
 
